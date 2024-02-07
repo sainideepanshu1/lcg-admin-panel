6ed508d9e81c7c2f0dffd36423a9a3d18f45c8e7
sainideepanshu1 <deepanshusaini.saini5@gmail.com>
2024-02-07 11:00:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
