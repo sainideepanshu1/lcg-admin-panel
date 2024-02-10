998775f124b5b085acef17c9d7a0fae9ce1a38b6
gauravsaini98966 <gs9042312@gmail.com>
2024-02-10 17:51:28 +0530

    Navbar Functionaly compleate

--- src/Data/Alertdata.jsx
+++ src/Data/Alertdata.jsx
@@ -1,5 +1,10 @@
-const Alert=[
+export const Alert=[
     {
-        
+        h3:"Your bill payment failed",
+        h4:"The    payment for your Shopify bill couldn't be  processed because of a problem with your payment method"
+    },
+    {
+        h3:"Your bill payment failed",
+        h4:"The    payment for your Shopify bill couldn't be  processed because of a problem with your payment method"
     }
 ]
