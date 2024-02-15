6aac1bbe5554862381bd787069f6256921b5e027
gauravsaini98966 <gs9042312@gmail.com>
2024-02-15 18:53:04 +0530

    new

--- src/Components/Customers.jsx
+++ src/Components/Customers.jsx
@@ -4,17 +4,54 @@ import { BsThreeDots } from "react-icons/bs";
 import { useState } from "react";
 import { useRef } from "react";
 import { FaAngleDown } from "react-icons/fa";
-// import { Link } from "react-router-dom";
+
 import { CgSortAz } from "react-icons/cg";
 import { IoChevronUp } from "react-icons/io5";
 import { LuLayout } from "react-icons/lu";
 import { IoFilterSharp } from "react-icons/io5";
+import { LuArrowUp } from "react-icons/lu";
+import { LuArrowDown } from "react-icons/lu";
+import { IoIosSearch } from "react-icons/io";
+import { LuArrowUpDown } from "react-icons/lu";
 function Customers() {
   const [toggle, setToggle] = useState(false);
+  const [filter, setFilter] = useState(false);
   const imexRef = useRef(null);
 
- 
+  const [sort, setSort] = useState(false);
+  const sortRef = useRef(null);
 
+  // ----sort bar---
+  const handleclick = (event) => {
+    if (sortRef.current && !sortRef.current.contains(event.target)) {
+      setSort(!sort);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleclick, true);
+
+    return () => {
+      document.removeEventListener("click", handleclick, true);
+    };
+  }, []);
+
+  // ---------filter click-------
+  function filterClick() {
+    setFilter(!filter);
+  }
+
+  // --------searchbar deny function--------------
+
+  const [textareaValue, setTextareaValue] = useState("");
+  const handleButtonClick = () => {
+    setTextareaValue(" ");
+  };
+
+  const handleTextareaChange = (event) => {
+    setTextareaValue(event.target.value);
+  };
+
+  // --------------import export outclick button--------
   const handleClickOutside = (event) => {
     if (imexRef.current && !imexRef.current.contains(event.target)) {
       setToggle(false);
@@ -28,6 +65,8 @@ function Customers() {
       document.removeEventListener("click", handleClickOutside, true);
     };
   }, []);
+
+  // top down arrow------------
 
   return (
     <div className="bg-[#F1F1F1] w-full h-screen justify-between ">
@@ -73,92 +112,237 @@ function Customers() {
       </div>
       {/* ---------- no of customer--------  */}
 
-      <div className=" bg-[ #F3F3F3] 
-      rounded-xl border-[1px] border-[#a09f9fdc] shadow-md my-[20px] mx-7   xm:mx-0 gap-0 justify-normal xm:rounded-md ">
-        <div    className=" flex items-center justify-between ">
-        <div className="flex items-center gap-0  flex-wrap xm:gap-0  ">
-          <div className=" flex  gap-1 text-heading py-[16px] px-[16px] xm:py-[4px]  ">
-            <h3 className="text-[#080808]">116020 </h3>
-            <h3 className="text-[#555454]">customers</h3>
+      <div
+        className=" bg-[ #F3F3F3] 
+      rounded-xl border-[1px] border-[#a09f9fdc] shadow-md my-[20px] mx-7   xm:mx-0 gap-0 justify-normal xm:rounded-md "
+      >
+        <div className=" flex items-center justify-between ">
+          <div className="flex items-center gap-0  flex-wrap xm:gap-0  ">
+            <div className=" flex  gap-1 text-heading py-[16px] px-[16px] xm:py-[4px]  ">
+              <h3 className="text-[#080808]">116020 </h3>
+              <h3 className="text-[#555454]">customers</h3>
+            </div>
+
+            <div className=" flex text-heading gap-1  py-[16px] px-[16px] xm:px-[10px] xm:py-[4px] ">
+              <h3 className="text-[#080808]">100% </h3>
+              <h3 className="text-[#555454]">of your customer base</h3>
+            </div>
           </div>
 
-          <div className=" flex text-heading gap-1  py-[16px] px-[16px] xm:px-[10px] xm:py-[4px] ">
-            <h3 className="text-[#080808]">100% </h3>
-            <h3 className="text-[#555454]">of your customer base</h3>
+          <div>
+            <div
+              onClick={filterClick}
+              className={`flex transition-400 px-[15px] ${
+                filter ? "hidden" : "block"
+              }`}
+            >
+              <button className="flex w-[89px] text-heading items-center px-[5px] py-[4px] gap-1 border-[1px]  bg-[#E3E3E3] hover:bg-[rgb(206,204,204)] rounded-lg ">
+                <span className="text-[#080808]">Add Filter</span>
+                <span>
+                  <FaAngleDown />
+                </span>
+              </button>
+            </div>
+            <div>
+              <div
+                className={` flex items-center gap-8 px-[10px] ${
+                  filter ? "block" : "hidden"
+                }`}
+              >
+                <div className="cursor-pointer">
+                  <div className="group  relative">
+                    <div className="hidden absolute bg-white p-2 rounded-lg text-heading top-[-40px] right-[-25px] group-hover:block">
+                      <h3>Templates</h3>
+                    </div>
+                    <div className=" rounded-md text-[#4a4a4a] p-2 hover:hover:bg-[#E3E3E3]  ">
+                      <span>
+                        <LuLayout />
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="relative group cursor-pointer text-[18px] border-l  border-r border-1 border-[#EBEBEB] px-[12px] ">
+                  <div className=" hidden absolute top-[-42px] right-[-2px] bg-white p-2 rounded-lg text-heading  group-hover:block">
+                    <h3>Filters</h3>
+                  </div>
+
+                  <div className="rounded-md  p-[6px] text-[#4a4a4a]   hover:bg-[#E3E3E3] ">
+                    <span className="">
+                      <IoFilterSharp />
+                    </span>
+                  </div>
+                </div>
+                <div onClick={filterClick}>
+                  <div className="relative group cursor-pointer">
+                    <div
+                      className=" w-[91px] hidden  absolute right-[-31px]
+  top-[-43px] bg-white p-2 rounded-lg text-heading  group-hover:block xm:right-[-2px]"
+                    >
+                      <h3>Close editor</h3>
+                    </div>
+                    <div className=" rounded-md text-[#4a4a4a]   p-[6px]   hover:bg-[#E3E3E3] ">
+                      <span>
+                        <IoChevronUp />
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        <div className={`${filter ? "block" : "hidden"}`}>
+          <div className="bg-white  ">
+            <textarea
+              placeholder="To create a segment, choose  template or apply a filter"
+              className="w-full py-2 font-mono px-2 text-[14px] text-[#303030] h-[80px]"
+              onChange={handleTextareaChange}
+              value={textareaValue}
+            ></textarea>
 
-        <div>
-          <div className=" hidden flex transition-400 px-[15px]">
-            <button className="flex w-[89px] text-heading items-center px-[5px] py-[4px] gap-1 border-[1px]  bg-[#E3E3E3] hover:bg-[rgb(206,204,204)] rounded-lg ">
-              <span className="text-[#080808]">Add Filter</span>
-              <span>
-                <FaAngleDown />
-              </span>
+            <hr></hr>
+          </div>
+
+          <div className="flex justify-end gap-2 py-[10px] px-[15px] bg-white rounded-b-xl">
+            <button
+              onClick={handleButtonClick}
+              className=" py-[6px] px-[8px] rounded-lg text-[13px] border-[1px]"
+            >
+              Discard
+            </button>
+            <button className=" py-[6px] px-[8px] rounded-lg text-[13px] border-[1px]">
+              Apply
             </button>
           </div>
-          <div className=" flex items-center gap-8 px-[10px]">
-            <div className="cursor-pointer">
-              <div className="group  relative">
-                <div
-                  
-                  className="hidden absolute bg-white p-2 rounded-lg text-heading top-[-40px] right-[-25px] group-hover:block"
-                >
-                  <h3>Templates</h3>
-                </div>
-                <div className=" rounded-md text-[#4a4a4a] p-2 hover:hover:bg-[#E3E3E3]  ">
-                  <span>
-                    <LuLayout />
-                  </span>
-                </div>
-              </div>
+        </div>
+      </div>
+      {/* ------------------customer detail----------- */}
 
-           
-            </div>
-            <div className="relative group cursor-pointer text-[18px] border-l  border-r border-1 border-[#EBEBEB] px-[12px] ">
-              <div className=" hidden absolute top-[-42px] right-[-2px] bg-white p-2 rounded-lg text-heading  group-hover:block">
-                <h3>Filters</h3>
-              </div>
-
-              <div className="rounded-md  p-[6px] text-[#4a4a4a]   hover:bg-[#E3E3E3] ">
-                <span className="">
-                <IoFilterSharp />
-                </span>
-              </div>
-            </div>
-
-            <div className=" relative group cursor-pointer">
-            <div className=" w-[91px] hidden  absolute right-[-31px]
-  top-[-43px] bg-white p-2 rounded-lg text-heading  group-hover:block xm:right-[-2px]">
-              <h3>Close editor</h3>
-            </div>
-              <div className=" rounded-md text-[#4a4a4a]   p-[6px]   hover:bg-[#E3E3E3] ">
-
-                
-                <span>
-                  <IoChevronUp />
-                </span>
-              </div>
-            </div>
+      <div className=" bg-white rounded-xl border-[1px] border-[#a09f9fdc] shadow-md my-[20px] mx-7   xm:mx-0 gap-0 justify-normal xm:rounded-md">
+        <div className=" flex justify-between p-[8px] ">
+          <div className="w-full justify-center flex items-center  py-1 px-[8px] rounded-lg bg-[#faf8f8] focus-within:outline focus-within:bg-[#f1f1f1c2]  ">
+            <span className="text-[#616161]">
+              <IoIosSearch />
+            </span>
+            <input
+              placeholder="Search customers"
+              className="w-full outline-none text-[14px] bg-[#faf8f8] text-[#303030] focus-within:bg-[#f1f1f1c2]"
+            />
           </div>
+
+          <div className="flex  items-center ">
+            <button
+              className=" bg-black text-[14px] text-white rounded-lg mx-[10px]
+          px-[7px] py-[5px]"
+            >
+              Search
+            </button>
+
+            <div>
+              <button
+                ref={sortRef}
+                onClick={() => {
+                  setSort(!sort);
+                }}
+                className=" relative border-[1px] rounded-md bg-white p-[4px]"
+              >
+                <LuArrowUpDown />
+              </button>
+            </div>
+
+            {sort && (
+              <div className=" ">
+                <div className="text-[#1a1a1a] flex flex-col gap-1 absolute top-[264px] right-[38px] px-[10px] rounded-lg py-[8px] bg-white text-[14px] xm:right-[10px]">
+                  <h1>Sort by</h1>
+                  <div className="flex gap-2 outline-[#000] ">
+                    {" "}
+                    <input
+                      className="text-[#1a1a1a]"
+                      type="radio"
+                      name="radio"
+                    />
+                    <span>Last update </span>
+                  </div>
+                  <div className="flex cursor-pointer gap-2">
+                    {" "}
+                    <input type="radio" value="HTML" name="radio" />
+                    <span>Amount spent </span>
+                  </div>
+                  <div className="flex gap-2">
+                    {" "}
+                    <input type="radio" name="radio" />
+                    <span>Total orders </span>
+                  </div>
+
+                  <div className="flex gap-2">
+                    {" "}
+                    <input type="radio" name="radio" />
+                    <span>Last order date</span>
+                  </div>
+
+                  <div className="flex gap-2">
+                    {" "}
+                    <input type="radio" name="radio" />
+                    <span>First order date </span>
+                  </div>
+
+                  <div className="flex gap-2">
+                    {" "}
+                    <input type="radio" name="radio" />
+                    <span>Date added as customer </span>
+                  </div>
+                  <div className="flex gap-2">
+                    {" "}
+                    <input type="radio" name="radio" />
+                    <span>Last abandoned order date </span>
+                  </div>
+
+                  <div className="">
+                    <div className=" cursor-pointer flex gap-2 items-center ">
+                      <span>
+                        <LuArrowUp />
+                      </span>
+                      <h3>Oldest to newest</h3>
+                    </div>
+
+                    <div className=" cursor-pointer  flex gap-2 items-center">
+                      <span>
+                        {" "}
+                        <LuArrowDown />
+                      </span>
+                      <h3>Newest to oldest</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
-         
         </div>
 
-        <div className="bg-white  ">
-       <textarea className="w-full h-[45px]">
-      
-       </textarea>
-       
-          <hr></hr>
-        </div>
+        <div
+          className="grid justify-between gap-4  bg-[#f5f3f3] text-[#686767] px-[10px] py-[8px] text-[13px] font-medium items-center"
+          style={{ gridTemplateColumns: "1fr 3fr 2fr 2fr 2fr 2fr" }}
+        >
+          {/* <div className=" flex gap-[150px]"> */}
+          {/* <div className="flex gap-3 items-center"> */}
+          <div>
+            <input type="checkbox" className="rounded " />
+          </div>
+          <h3>Customer name</h3>
+          {/* </div> */}
+          {/* <div> */}
+          <h3>Email subscription</h3>
+          {/* </div> */}
+          {/* <div> */}
+          <h3>Location</h3>
+          {/* </div> */}
+          {/* </div> */}
 
-        <div className="flex justify-end gap-2 py-[10px] px-[15px] bg-white rounded-b-xl">
-            <button className=" py-[6px] px-[8px] rounded-lg text-[13px] border-[1px]">Discard</button>
-            <button className=" py-[6px] px-[8px] rounded-lg text-[13px] border-[1px]">Apply</button>
+          {/* <div className="flex gap-[70px] items-center pr-[50px]"> */}
+          <h3>Orders</h3>
+          <h3>Amount spent</h3>
+          {/* </div> */}
         </div>
-
-       
       </div>
     </div>
   );
