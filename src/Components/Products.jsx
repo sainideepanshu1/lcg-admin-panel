41c84cdf1dc554b9c8ff41343148d25e7e1d8f9a
Ajay Verma <ajaygsss1@gmail.com>
2024-02-19 16:12:23 +0530

    update product

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -87,9 +87,9 @@ const Products = () => {
               </button>
             </div>
           </div>
-          <div className="rounded-lg mt-[24px] xm:p-0 bg-[#ffffff] overflow-x: auto; scrollbar-width: none; w-full gap-4 justify-between flex flex-col border border-stone-200">
+          <div className="rounded-lg mt-[24px] xm:p-0 bg-[#ffffff]  w-full gap-4 justify-between flex flex-col border border-stone-200">
             <div className="flex justify-between py-1 px-1 text-[#585858] ">
-              <div className="flex overflow-x-auto items-center ">
+              <div className="flex  sm:overflow-y-auto  items-center ">
                 <button className="hover:bg-[#E3E3E3] rounded-lg p-2 text-heading xm:text-[12px] font-[600] ">
                   All
                 </button>
@@ -167,44 +167,44 @@ const Products = () => {
               </div>
             </div>
             <div>
-              <div className="grid sm:overflow-auto sm:scroll-m-2 w-full ">
-                <div className="w-full px-2 grid xm:hidden   items-center text-[14px] border border-y-gray-300 text-[#666161] bg-[#f1f1f1] pt-2 pb-2">
+              <div className="grid  w-full ">
+                <div className="w-full px-2 grid xm:hidden   items-center  border border-y-gray-300 text-[#666161] bg-[#f1f1f1] pt-2 pb-2">
                   <div
-                    className="grid gap-12  items-center "
+                    className="grid items-center "
                     style={{
                       gridTemplateColumns:
-                        " 0fr 0.5fr 1.5fr 0.5fr 1.5fr 1fr 0.5fr 1fr 0.5fr",
+                        " 0fr 1.5fr 1.5fr 1fr 1.5fr 1fr 1fr 1fr 0.5fr",
                     }}
                   >
-                    <div className="text-center sm:fixed  flex items-center">
-                      <input className="h-4  sm:static w-4" type="checkbox" />
+                    <div className="text-center  flex items-center">
+                      <input className="h-4  w-4" type="checkbox" />
 
                     </div>
                     <div></div>
-                    <div className="flex font-[600] gap-2 items-center text-heading  group ">
+                    <div className="flex font-[600] gap-2 items-center text-[12px]  group ">
                       Product
                       <div className="flex-col hidden  text-[8px] group-hover:flex cursor-pointer  ">
                         <SlArrowUp /> <SlArrowDown />
                       </div>
                     </div>
-                    <div className="text-heading  font-[600] ">Status </div>
-                    <div className="text-heading font-[600]  flex gap-2 items-center group cursor-pointer  ">
+                    <div className="text-[12px]  font-[600] ">Status </div>
+                    <div className="text-[12px] font-[600]  flex gap-2 items-center group cursor-pointer  ">
                       Inventory
                       <div className="flex-col hidden  text-[8px] group-hover:flex">
                         <SlArrowUp /> <SlArrowDown />
                       </div>
                     </div>
-                    <div className="text-heading font-[600] ">
+                    <div className="text-[12px] font-[600] ">
                       Sales channels
                     </div>
-                    <div className="text-heading font-[600] ">Markets</div>
-                    <div className="text-heading font-[600] flex gap-2 items-center group  cursor-pointer ">
+                    <div className="text-[12px] font-[600] ">Markets</div>
+                    <div className="text-[12px] font-[600] flex gap-2 items-center group  cursor-pointer ">
                       Category
                       <div className="flex-col text-[8px] hidden group-hover:flex">
                         <SlArrowUp /> <SlArrowDown />
                       </div>
                     </div>
-                    <div className="text-heading font-[600] flex gap-2 group items-center cursor-pointer ">
+                    <div className="text-[12px] font-[600] flex gap-2 group items-center cursor-pointer ">
                       Vendor
                       <div className="flex-col hidden  text-[8px] group-hover:flex">
                         <SlArrowUp /> <SlArrowDown />
@@ -219,41 +219,41 @@ const Products = () => {
                       className="w-full px-2 border-l-0  grid border border-y-gray-100 xm:hidden items-center text-[14px] pt-2 pb-2"
                     >
                       <div
-                        className="grid gap-11 items-center "
+                        className="grid  items-center "
                         style={{
                           gridTemplateColumns:
-                            " 0fr 0.5fr 1.5fr 0.5fr 1.5fr 1fr 0.5fr 1fr 0.5fr",
+                            " 0.5fr 0.5fr 2.0fr 1.1fr 1.5fr 1fr 1fr 1fr 0.5fr",
                         }}
                       >
                         <div className="text-center flex items-center">
-                          <input className="h-4 sm:fixed w-4" type="checkbox" />
+                          <input className="h-4  w-4" type="checkbox" />
                         </div>
 
                         <div>
-                          <img className="h-[45px] w-[45px] sm:fixed rounded-xl " src={Pic2} alt="Pic" />
+                          <img className="h-[45px] w-[45px]  rounded-xl " src={Pic2} alt="Pic" />
                         </div>
-                        <div className="flex font-[450] gap-2 text-[#30304b] items-center text-heading group ">
+                        <div className="flex font-[450] gap-2 text-[#30304b] items-center text-[12px] group ">
                           <Link to={`/product/${product._id}`}>
                             {product.title}
                           </Link>
                           <div className="flex-col hidden  text-[8px] group-hover:flex   "></div>
                         </div>
-                        <div className="text-heading bg-[#B4FED2] w-14 capitalize text-[#5FA681] px-2 py-0 border rounded-xl font-[450] ">
+                        <div className="text-[12px] bg-[#B4FED2] w-14 capitalize text-[#5FA681] px-2 py-0 border rounded-xl font-[450] ">
                           {product.status}
                         </div>
-                        <div className="text-heading font-[450] text-[#666161] flex gap-2 items-center group ">
+                        <div className="text-[12px] font-[450] text-[#666161] flex gap-2 items-center group ">
                           Inventory
                           <div className="flex-col hidden  text-[#666161] text-[8px] group-hover:flex"></div>
                         </div>
-                        <div className="text-heading text-[#666161] font-[450] ">
+                        <div className="text-[12px] text-[#666161] font-[450] ">
                           Sales channels
                         </div>
-                        <div className="text-heading text-[#666161] font-[450] ">Markets</div>
-                        <div className="text-heading font-[450] text-[#666161] flex gap-2 items-center group  cursor-pointer ">
+                        <div className="text-[12px] text-[#666161] font-[450] ">Markets</div>
+                        <div className="text-[12px] font-[450] text-[#666161] flex gap-2 items-center group  cursor-pointer ">
                           {product.product_type}
                           <div className="flex-col text-[8px] text-[#666161] hidden group-hover:flex"></div>
                         </div>
-                        <div className="text-heading font-[450] text-[#666161] flex gap-2 group items-center cursor-pointer ">
+                        <div className="text-[12px] font-[450] text-[#666161] flex gap-2 group items-center cursor-pointer ">
                           {product.vendor}
                           <div className="flex-col hidden  text-[#666161] text-[8px] group-hover:flex"></div>
                         </div>
@@ -275,19 +275,19 @@ const Products = () => {
                           <img className="h-[45px] w-[45px] rounded-xl " src={Pic} alt="Pic" />
                         </div>
                         <div>
-                          <div className="flex font-[450] gap-2 items-center text-heading group ">
+                          <div className="flex font-[450] gap-2 items-center text-[14px] group ">
                             <Link to={`/product/${product._id}`}>
                               {product.title}
                             </Link>
                           </div>
-                          <div className="text-[12px] text-[#666161] font-[450] flex gap-2 items-center group ">
+                          <div className="text-[14px] text-[#666161] font-[450] flex gap-2 items-center group ">
                             Inventory
 
                           </div>
-                          <div className="text-[12px] text-[#666161] font-[450] flex gap-2 group items-center cursor-pointer ">
+                          <div className="text-[14px] text-[#666161] font-[450] flex gap-2 group items-center cursor-pointer ">
                             {product.vendor}
                           </div>
-                          <div className="text-[12px] bg-[#B4FED2] w-14 capitalize text-[#5FA681] px-2 py-0 border rounded-xl font-[450] ">
+                          <div className="text-[14px] bg-[#B4FED2] w-14 capitalize text-[#5FA681] px-2 py-0 border rounded-xl font-[450] ">
                             {product.status}
                           </div>
                         </div>
