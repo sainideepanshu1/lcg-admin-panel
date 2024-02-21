68ecfce21d413cf2e80de177900b092b75d5eb08
gauravsaini98966 <gs9042312@gmail.com>
2024-02-21 17:19:26 +0530

    products page on progrash

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -6,11 +6,13 @@ import { Link } from "react-router-dom";
 import { BsThreeDots } from "react-icons/bs";
 import axios from "axios";
 import { TbArrowsSort } from "react-icons/tb";
-
+import { IoIosSearch } from "react-icons/io";
 import Pic2 from "../assets/Product2.jpg";
 import { FaLongArrowAltDown } from "react-icons/fa";
 import { FaLongArrowAltUp } from "react-icons/fa";
 import { useRef } from "react";
+
+import { ImCancelCircle } from "react-icons/im";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -115,9 +117,9 @@ const Products = () => {
               </button>
             </div>
           </div>
-          <div className="rounded-lg mt-[24px]  xm:p-0 bg-[#ffffff] h-[80vh] overflow-x-hidden  w-full gap-4 flex flex-col border border-stone-200">
-            <div className="flex justify-between py-1 px-1 text-[#585858] ">
-              <div className="flex  sm:overflow-y-auto  items-center ">
+          <div className="rounded-lg mt-[24px]  xm:p-0 bg-[#ffffff] h-[80vh] overflow-x-hidden  w-full  flex flex-col border border-stone-200">
+            <div className="flex justify-between  py-3 px-1 text-[#585858] ">
+              <div className="flex   sm:overflow-y-auto  items-center ">
                 <button className="hover:bg-[#E3E3E3] rounded-lg p-2 text-heading xm:text-[12px] font-[600] ">
                   All
                 </button>
@@ -140,8 +142,31 @@ const Products = () => {
                   </Link>
                 </button>
               </div>
+            
+                <div className=" hidden flex w-full gap-3    ">
+                  <div className=" w-full    py-1 px-[8px] rounded-lg bg-[#faf8f8] focus-within:outline focus-within:bg-[#f1f1f1c2]  flex gap-2 items-center">
+                    <div className="w-full flex items-center">
+                    <span className="text-[#616161]">
+                      <IoIosSearch />
+                    </span>
+                    <input
+                      placeholder="Searching all products "
+                      className="w-full outline-none text-[14px] bg-[#faf8f8] text-[#303030] focus-within:bg-[#f1f1f1c2]"
+                    />
+                    </div>
+
+                      <span className=" cursor-pointer"><ImCancelCircle /></span>
+                  
+                  </div>
+                  <div className="flex gap-2 ">
+                  <button className=" bg-black text-[14px] text-white rounded-lg 
+          px-[7px] py-[5px]">Cancel</button>
+                  <button className=" bg-black text-[14px] text-white rounded-lg mr-[5px]
+          px-[7px] py-[5px]">Search</button>
+                </div>
+              </div>
               <div className="flex gap-1">
-                <button className="border shadow-lg hover:bg-[#e3e3e3] transition-all xm:text-[12px] text-[18px] px-2 py-0 rounded-lg flex items-center">
+                <button  className="  border shadow-lg hover:bg-[#e3e3e3] transition-all xm:text-[12px] text-[18px] px-2 py-0 rounded-lg flex items-center">
                   <IoSearchOutline /> <CgSortAz />
                 </button>
 
@@ -226,7 +251,7 @@ const Products = () => {
               <div className="grid overflow-auto  w-full ">
                 <div className="w-full px-2 grid xm:hidden  items-center  border border-y-gray-300 text-[#666161] bg-[#f1f1f1] pt-2 pb-2">
                   <div
-                    className="grid  items-center table-heading "
+                    className="destop_type  grid  items-center table-heading "
                     // style={{
                     //   gridTemplateColumns: "1.5fr 1.5fr ",
                     // }}
@@ -302,7 +327,7 @@ const Products = () => {
                           </div>
                         </div>
                         <div className="flex items-center  gap-4">
-                          <div className="flex font-[450] gap-2 w-[200px] text-[#30304b] items-center pl-2 pr-2 text-[12px] group ">
+                          <div className="flex font-[450] gap-2 w-[200px] text-[#000] items-center pl-2 pr-2  text-[12px] hover:underline group ">
                             <Link to={`/product/${product._id}`}>
                               {product.title}
                             </Link>
@@ -359,7 +384,7 @@ const Products = () => {
                             {product.vendor}
                           </div>
                           <div>
-                            <div className="text-[14px] bg-[#B4FED2] w-[100px] capitalize text-[#5FA681] border rounded-xl font-[450] ">
+                            <div className="text-[14px] bg-[#B4FED2] w-[80px]  px-4 capitalize text-[#0c5132] border rounded-xl font-[450] ">
                               {product.status}
                             </div>
                           </div>
