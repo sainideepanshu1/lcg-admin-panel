576020ebd2f6f43dd53f73873532fa8cd5c98baf
gauravsaini98966 <gs9042312@gmail.com>
2024-02-21 12:37:30 +0530

    Product responsive

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -35,7 +35,6 @@ const Products = () => {
 
   // -------------sort ------
 
-
   const handleSortClick = (event) => {
     if (sortref.current && !sortref.current.contains(event.target)) {
       setSort(false);
@@ -116,7 +115,7 @@ const Products = () => {
               </button>
             </div>
           </div>
-          <div className="rounded-lg mt-[24px] xm:p-0 bg-[#ffffff] h-[144vh] overflow-x-hidden  w-full gap-4 justify-between flex flex-col border border-stone-200">
+          <div className="rounded-lg mt-[24px]  xm:p-0 bg-[#ffffff] h-[80vh] overflow-x-hidden  w-full gap-4 flex flex-col border border-stone-200">
             <div className="flex justify-between py-1 px-1 text-[#585858] ">
               <div className="flex  sm:overflow-y-auto  items-center ">
                 <button className="hover:bg-[#E3E3E3] rounded-lg p-2 text-heading xm:text-[12px] font-[600] ">
@@ -224,15 +223,15 @@ const Products = () => {
               </div>
             </div>
             <div>
-              <div className="grid sm:overflow-auto  w-full ">
+              <div className="grid overflow-auto  w-full ">
                 <div className="w-full px-2 grid xm:hidden  items-center  border border-y-gray-300 text-[#666161] bg-[#f1f1f1] pt-2 pb-2">
                   <div
-                    className="grid  items-center "
-                    style={{
-                      gridTemplateColumns: "0.3fr 1.5fr ",
-                    }}
+                    className="grid  items-center table-heading "
+                    // style={{
+                    //   gridTemplateColumns: "1.5fr 1.5fr ",
+                    // }}
                   >
-                    <div className=" flex gap-8  sm:sticky sm:left-0 border-y-gray-300 text-[#666161] bg-[#f1f1f1] w-[100px] ">
+                    <div className=" flex gap-8  sticky left-0 border-y-gray-300 text-[#666161] bg-[#f1f1f1] w-[100px] ">
                       <div className="text-center  flex items-center">
                         <input className="h-4  w-4" type="checkbox" />
                       </div>
@@ -290,7 +289,7 @@ const Products = () => {
                           gridTemplateColumns: " 1.5fr 1.5fr ",
                         }}
                       >
-                        <div className=" flex gap-6 sm:sticky sm:left-0 bg-white border-y-gray-300 text-[#666161]  w-[100px] ">
+                        <div className=" flex gap-6 sticky left-0 bg-white border-y-gray-300 text-[#666161]  w-[100px] ">
                           <div className="text-center  flex items-center">
                             <input className="h-4  w-4" type="checkbox" />
                           </div>
@@ -302,13 +301,13 @@ const Products = () => {
                             />
                           </div>
                         </div>
-                        <div className="flex  gap-4">
+                        <div className="flex items-center  gap-4">
                           <div className="flex font-[450] gap-2 w-[200px] text-[#30304b] items-center pl-2 pr-2 text-[12px] group ">
                             <Link to={`/product/${product._id}`}>
                               {product.title}
                             </Link>
                           </div>
-                          <div className="text-[12px] w-[100px] bg-[#B4FED2]  capitalize text-[#5FA681] text-center  border rounded-xl  p-1 ">
+                          <div className="text-[12px] w-[100px] h-[27px] bg-[#B4FED2]  capitalize text-[#0c5132] text-center  border rounded-xl  p-1 ">
                             {product.status}
                           </div>
                           <div className="text-[12px] w-[100px] font-[450] text-[#666161] flex gap-2 items-center group ">
@@ -339,11 +338,14 @@ const Products = () => {
                       className="w-full border-l-0 border-r-0 px-2 pl-[1rem] hidden xm:block lg:hidden items-center border border-y-gray-100 text-[#30304b] text-[0.813rem] pt-3 pb-3"
                     >
                       <div className="flex gap-4">
-                        <div> <img
-                              className=" w-[45px] object-contain  rounded-xl "
-                              src={Pic2}
-                              alt="Pic"
-                            /></div>
+                        <div>
+                          {" "}
+                          <img
+                            className=" w-[45px] object-contain  rounded-xl "
+                            src={Pic2}
+                            alt="Pic"
+                          />
+                        </div>
                         <div>
                           <div className="flex font-[450] gap-2 items-center text-[14px] group ">
                             <Link to={`/product/${product._id}`}>
