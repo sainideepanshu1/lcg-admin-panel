1e79f4385afa087612017245a467b030c2c881a3
gauravsaini98966 <gs9042312@gmail.com>
2024-02-27 11:22:38 +0530

    customer_Detail compleate

--- src/Components/CustomerDetails.jsx
+++ src/Components/CustomerDetails.jsx
@@ -1,21 +1,52 @@
 import React, { useEffect, useRef, useState } from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
-import { LuDot } from "react-icons/lu";
-import { RxDotFilled } from "react-icons/rx";
+
 import { FiChevronDown } from "react-icons/fi";
 import { MdCurrencyRupee } from "react-icons/md";
 import { IoCheckbox } from "react-icons/io5";
 import { MdCheckBoxOutlineBlank } from "react-icons/md";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import Product2 from '../assets/Product2.jpg'
+import Product2 from "../assets/Product2.jpg";
 import { LuClipboard } from "react-icons/lu";
 import { Link } from "react-router-dom";
-
+import { LuPencil } from "react-icons/lu";
+import { BsEmojiSmile } from "react-icons/bs";
+import { CiAt } from "react-icons/ci";
+import { FiHash } from "react-icons/fi";
 import { CiShop } from "react-icons/ci";
+// import { ImAttachment } from "react-icons/im";
+// import copy from "copy-to-clipboa";
+// import copy from "copy-to-clipboard";
+import { GrSquare } from "react-icons/gr";
 
 function CustomerDetails() {
   const [action, setaction] = useState();
   const actionref = useRef();
+
+  const [Customer, setCustomer] = useState();
+  const CustomerRef = useRef();
+
+  // ---------------emoji picker code----------
+  const [inputStr, setInputStr] = useState("");
+  const [showPicker, setShowPicker] = useState(false);
+
+  const onEmojiClick = (event, emojiObject) => {
+    setInputStr((prevInput) => prevInput + emojiObject.emoji);
+    setShowPicker(false);
+  };
+
+  // ------------code clipbord------\
+
+  const [copyText, setCopyText] = useState("");
+
+  const handleCopyText = (e) => {
+    setCopyText(e.target.value);
+  };
+
+  const copyToClipboard = () => {
+    copy(copyText);
+    alert(`You have copied "${copyText}"`);
+  };
 
   // ----------more-action-------------
   const handleaction = (event) => {
@@ -32,21 +63,21 @@ function CustomerDetails() {
     };
   }, []);
 
-  // const handleClickOutside = (event) => {
-  //   if (imexRef.current && !imexRef.current.contains(event.target)) {
-  //     setToggle(false);
-  //   }
-  // };
+  // ---------------customer---------------
+  const CustomerClick = (event) => {
+    if (CustomerRef.current && !CustomerRef.current.contains(event.target)) {
+      setCustomer(false);
+    }
+  };
 
-  // useEffect(() => {
-  //   document.addEventListener("click", handleClickOutside, true);
-
-  //   return () => {
-  //     document.removeEventListener("click", handleClickOutside, true);
-  //   };
-  // }, []);
+  useEffect(() => {
+    document.addEventListener("click", CustomerClick);
+    return () => {
+      document.removeEventListener("click", CustomerClick);
+    };
+  }, []);
   return (
-    <div className="bg-[#F1F1F1] w-full h-[100vh] :px-[100px] md:px[10px]">
+    <div className="bg-[#F1F1F1] w-full h-[100%] :px-[100px] md:px[10px]">
       <div className="flex my-6 items-center justify-between px-7 py-1 sm:px-6 xm:px-2 ">
         <div>
           {" "}
@@ -82,7 +113,7 @@ function CustomerDetails() {
           </button>
           {action && (
             <div
-              className=" flex flex-col gap-2 absolute right-[28px]
+              className=" z-10 flex flex-col gap-2 absolute right-[28px]
   top-[130px] text-[13px] text-[#303030] bg-white items-start py-[10px] px-[10px] rounded-lg "
             >
               <button className=" py-[5px] px-[6px] hover:bg-[#E3E3E3]  rounded-lg">
@@ -177,110 +208,279 @@ function CustomerDetails() {
               <div className=" text-[#616161] text-[13px] pt-[5px] pb-[15px] px-[10px] ">
                 <h2>23 February 2024 at 10:02 am from Online Store</h2>
               </div>
-             <hr/>
+              <hr />
 
-             <div className="flex justify-between items-start px-[10px] pt-[15px]">
-              <div className="flex items-center gap-[10px]">
-                <div className=" w-[43px] h-[40px] object-contain ">
-                  <img className="rounded-md" src={Product2} alt="" />
-                </div>
-                <div className="">
-                  <div className="text-[13px] font-semibold">
-                  <h2>Glowing Panda - A Memorable Gift</h2>
+              <div className="flex justify-between items-start px-[10px] pt-[15px]">
+                <div className="flex items-center gap-[10px]">
+                  <div className=" w-[43px] h-[40px] object-contain ">
+                    <img className="rounded-md" src={Product2} alt="" />
                   </div>
+                  <div className="">
+                    <div className="text-[13px] font-semibold">
+                      <h2>Glowing Panda - A Memorable Gift</h2>
+                    </div>
 
-                  <div className="text-[13px] text-[#303030] bg-[#e3e3e3] px-2 my-[4px] rounded-lg line-clamp-1">
-                  <h3 className="line-clamp-1">Brown / Without Gift Wrap / Without Gift Card</h3>
+                    <div className="text-[13px] text-[#303030] bg-[#e3e3e3] px-2 my-[4px] rounded-lg line-clamp-1">
+                      <h3 className="line-clamp-1">
+                        Brown / Without Gift Wrap / Without Gift Card
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex items-center  gap-6">
+                  <div className="text-[13px] xm:hidden">x1</div>
+
+                  <div className="flex items-center text-[14px] font-semibold ">
+                    <span className="flex items-center">
+                      <MdCurrencyRupee />
+                      649
+                    </span>{" "}
+                    <span>.00</span>
                   </div>
                 </div>
               </div>
-
-              <div className="flex items-center  gap-6">
-                <div className="text-[13px] xm:hidden">x1</div>
-
-              <div className="flex items-center text-[14px] font-semibold ">
-                  <span className="flex items-center">
-                    <MdCurrencyRupee />
-                    649
-                  </span>{" "}
-                  <span>.00</span>
-                </div>
-
-              </div>
-
-             </div>
-            
-
-
-
             </div>
             <div className=" flex justify-end gap-2">
               <div className="">
-                <Link to='/'>
-                <button className="text-[13px] border-[1px] px-[10px] py-1 rounded-lg text-[#303030] font-semibold">View all orders</button>
+                <Link to="/">
+                  <button className="text-[13px] border-[1px] px-[10px] py-1 rounded-lg text-[#303030] font-semibold">
+                    View all orders
+                  </button>
                 </Link>
               </div>
               <div>
-                <Link to='/'>
-                <button className="text-[13px] border-[1px] px-[10px] py-1 rounded-lg text-[white] font-semibold bg-black">Create order</button>
+                <Link to="/">
+                  <button className="text-[13px] border-[1px] px-[10px] py-1 rounded-lg text-[white] font-semibold bg-black">
+                    Create order
+                  </button>
                 </Link>
               </div>
-
             </div>
-            
-            
           </div>
-        </div>
-        <div className="w-[30%]  xl:w-[100%]  border-[1px bg-white py-[20px] px-[15px]	 shadow-md rounded-[8px]">
-          <div className="flex justify-between  items-center ">
-            <h3 className="text-[13px] font-semibold">Customer</h3>
-            <div>
-              <div className="cursor-pointer py-1 px-[6px] transition ease-in-out rounded-md hover:bg-[#d8d8d8]">
-                <span className="">
-                  <BsThreeDotsVertical />
-                </span>
+
+          <div className="">
+            <h3 className="text-[14px] font-semibold text-[#303030] py-1">
+              Timeline
+            </h3>
+            <div className=" border-[1px]  bg-white pt-[15px]	 shadow-md rounded-[8px]">
+              <div className=" flex items-center px-[10px]  ">
+                <div className="w-[45px] h-[45px] ">
+                  <img className="rounded-lg" src={Product2} />
+                </div>
+
+                <div className="w-full outline-none mx-[8px]">
+                  <input
+                    className="w-full outline-none text-[14px] "
+                    // type="text"
+                    placeholder="Leave a comment...."
+                  />
+                </div>
               </div>
 
-              <div className=""></div>
-            </div>
-          </div>
-          <h2 className="text-[13px] font-semibold py-[8px]">
-            Contact information
-          </h2>
-
-          <div className="flex justify-between items-center">
-            <div>
-              <span className="text-[#005bd3] text-[13px] hover:underline">
-                Gauravsaini@gmail.com
-              </span>
-            </div>
-            <div>
-              <span className="text-[#616161] font-semibold">
-                <LuClipboard />
-              </span>
-            </div>
-          </div>
-          <div className="text-[13px]">
-            <h3 className="text-[13px]">No email address provided</h3>
-            <span className="py-[2px]">+91 82787 57344</span>
-            <h3 className="py-[2px]">Will receive notifications in English</h3>
-          </div>
-
-          <div className="py-[8px]">
-            <h3 className="text-[13px] font-semibold">Default address</h3>
-            <div className="text-[13px] py-[8px] flex flex-col gap-[2px]">
-              <h2>Deepak Roy</h2>
-              <h2>2</h2>
-              <h2>
-                Mandir complex Back Side Road 792001 TEZU Arunachal Pradesh
-                India
+              <div className="flex flex-wrap justify-between bg-[#f7f7f7] px-4 py-5  rounded-b-[8px]">
+                <div className="flex items-center gap-3 text-[16px] text-[#303030]">
+                  <div className="">
+                    <button className="text-[#696666]">
+                      <BsEmojiSmile />
+                    </button>
+                    {/* <div className="absolute"> */}
+                    {/* {showPicker && (
+          <Picker className="absolute" pickerStyle={{ width: "30%" }} onEmojiClick={onEmojiClick}  />
+        )} */}
+                    {/* </div> */}
+                  </div>
+                  <div>
+                    <button className="text-[#696666]">
+                      <CiAt />
+                    </button>
+                  </div>
+                  <div>
+                    <button className="text-[#696666]">
+                      <FiHash />
+                    </button>
+                  </div>
+                  <div className="pb-[6px]">
+                    <input className="text-[14px]" type="file"></input>
+                  </div>
+                </div>
+                <div>
+                  <button className="text-[13px] border-[1px] px-[10px] py-1 rounded-lg text-[#303030] font-semibold">
+                    Post
+                  </button>
+                </div>
+              </div>
+              <h2 className="text-[#616161] text-[13px] float-right p-[2px] font-medium">
+                Only you and other staff can see comments
               </h2>
             </div>
+
+            <div className="border-l-[2.5px] relative  max-h-[800px] ml-10  border-[#a5a4a4] top-0">
+              <div className=" flex flex-col gap-6 py-[40px] ">
+                <div className="flex flex-col">
+                  <h3 className="px-[20px] text-[#616161] text-[13px]"   >Yesterday</h3>
+                  <div className="flex items-center px-[20px] py-[10px]   ">
+                    <span className="absolute left-[-10px] text-[#747373]"><GrSquare /></span>
+                    <div className="flex gap-[30px]">
+                    <h3 className="text-[#303030] text-[13px]">Order Confirmation email for order <Link><button className=" bg-[#e3e3e3] font-semibold rounded-lg py-[2px] px-[4px] text-[#303030] ">#42544</button></Link> sent to this customer (Aroramonica4@gmail.com).</h3>
+                    <span className="flex  text-[#303030] text-[14px]">5.22 pm</span>
+                    </div>
+                  </div>
+                </div>
+
+
+
+                <div className="flex flex-col">
+                  <h3 className="px-[20px] text-[#616161] text-[13px]"   >Yesterday</h3>
+                  <div className="flex items-center px-[20px] py-[10px]   ">
+                    <span className="absolute left-[-10px] text-[#747373]"><GrSquare /></span>
+                    <div className="flex gap-[30px]">
+                    <h3 className="text-[#303030] text-[13px]">Order Confirmation email for order <Link><button className=" bg-[#e3e3e3] font-semibold rounded-lg py-[2px] px-[4px] text-[#303030] ">#42544</button></Link> sent to this customer (Aroramonica4@gmail.com).</h3>
+                    <span className="flex  text-[#303030] text-[14px]">5.22 pm</span>
+                    </div>
+                  </div>
+                </div>
+
+
+
+                <div className="flex flex-col">
+                  <h3 className="px-[20px] text-[#616161] text-[13px]"   >Yesterday</h3>
+                  <div className="flex items-center px-[20px] py-[10px]   ">
+                    <span className="absolute left-[-10px] text-[#747373]"><GrSquare /></span>
+                    <div className="flex gap-[30px]">
+                    <h3 className="text-[#303030] text-[13px]">Order Confirmation email for order <Link><button className=" bg-[#e3e3e3] font-semibold rounded-lg py-[2px] px-[4px] text-[#303030] ">#42544</button></Link> sent to this customer (Aroramonica4@gmail.com).</h3>
+                    <span className="flex  text-[#303030] text-[14px]">5.22 pm</span>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className="flex flex-col">
+                  <h3 className="px-[20px] text-[#616161] text-[13px]"   >Yesterday</h3>
+                  <div className="flex items-center px-[20px] py-[10px]   ">
+                    <span className="absolute left-[-10px] text-[#747373]"><GrSquare /></span>
+                    <div className="flex gap-[30px]">
+                    <h3 className="text-[#303030] text-[13px]">Order Confirmation email for order <Link><button className=" bg-[#e3e3e3] font-semibold rounded-lg py-[2px] px-[4px] text-[#303030] ">#42544</button></Link> sent to this customer (Aroramonica4@gmail.com).</h3>
+                    <span className="flex  text-[#303030] text-[14px]">5.22 pm</span>
+                    </div>
+                  </div>
+                </div>
+
+              
+
+
+
+              
+                
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-[30%] xl:w-[100%]  flex flex-col gap-[15px]">
+          <div className="    border-[1px bg-white py-[20px] px-[15px]	 shadow-md rounded-[8px]">
+            <div className="flex justify-between  items-center ">
+              <h3 className="text-[13px] font-semibold">Customer</h3>
+              <div className="relative">
+                <div
+                  ref={CustomerRef}
+                  onClick={() => {
+                    setCustomer(!Customer);
+                  }}
+                  className="  cursor-pointer py-1 px-[6px] transition ease-in-out rounded-md hover:bg-[#d8d8d8]"
+                >
+                  <span className="">
+                    <BsThreeDotsVertical />
+                  </span>
+                </div>
+                {Customer && (
+                  <div className=" border-[1px]  w-[220px] z-10 flex flex-col gap-2 absolute right-[0px] top-[38px] text-[13px] text-[#303030] bg-white items-start py-[10px] px-[10px] rounded-lg ">
+                    <button className=" py-[5px] px-[6px] hover:bg-[#E3E3E3]  rounded-lg">
+                      Edit contact information
+                    </button>
+                    <button className="py-[5px] px-[6px] hover:bg-[#E3E3E3]  rounded-lg">
+                      Manage addresses{" "}
+                    </button>
+                    <button className="py-[5px] px-[6px] hover:bg-[#E3E3E3]  rounded-lg">
+                      Edit marketing settings
+                    </button>
+                    <button className="py-[5px] px-[6px] hover:bg-[#E3E3E3]  rounded-lg">
+                      Manage tax exemptions
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+            <h2 className="text-[13px] font-semibold py-[8px]">
+              Contact information
+            </h2>
+
+            <div className="flex justify-between items-center">
+              <div>
+                <span className="text-[#005bd3] text-[13px] hover:underline">
+                  Gauravsaini@gmail.com
+                </span>
+              </div>
+              <div>
+                <span className="text-[#616161] font-semibold cursor-pointer">
+                  <LuClipboard />
+                </span>
+              </div>
+            </div>
+            <div className="text-[13px]">
+              <h3 className="text-[13px]">No email address provided</h3>
+              <span className="py-[2px]">+91 82787 57344</span>
+              <h3 className="py-[2px]">
+                Will receive notifications in English
+              </h3>
+            </div>
+
+            <div className="py-[8px]">
+              <h3 className="text-[13px] font-semibold">Default address</h3>
+              <div className="text-[13px] py-[8px] flex flex-col gap-[2px]">
+                <h2>Deepak Roy</h2>
+                <h2>2</h2>
+                <h2>
+                  Mandir complex Back Side Road 792001 TEZU Arunachal Pradesh
+                  India
+                </h2>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <h3 className="text-[13px] font-semibold">Tax exemptions</h3>
+              <h3 className="text-[13px] text-[#616161]">No exemptions</h3>
+            </div>
+          </div>
+          <div className=" xl:w-[100%]  border-[1px bg-white py-[20px] px-[15px]	 shadow-md rounded-[8px]">
+            <div>
+              <div className="flex justify-between items-center">
+                <h3 className="text-[13px] font-semibold ">Tags</h3>
+                <span className="text-[#616161] cursor-pointer font-semibold">
+                  <LuPencil />
+                </span>
+              </div>
+              <div className="w-full py-2  ">
+                <input
+                  type="input"
+                  className="w-full border-[1px] border-[#303030] px-[8px] py-[2px] rounded-lg "
+                />
+              </div>
+            </div>
           </div>
 
-          <div className="flex flex-col gap-2">
-            <h3 className="text-[13px] font-semibold">Tax exemptions</h3>
-            <h3 className="text-[13px] text-[#616161]">No exemptions</h3>
+          <div className=" xl:w-[100%]  border-[1px bg-white py-[20px] px-[15px]	 shadow-md rounded-[8px]">
+            <div>
+              <div className="flex justify-between items-center">
+                <h3 className="text-[13px] font-semibold ">Notes</h3>
+                <span className="text-[#616161] cursor-pointer font-semibold">
+                  <LuPencil />
+                </span>
+              </div>
+              <div>
+                <h3 className="text-[13px] text-[#616161]">No notes</h3>
+              </div>
+            </div>
           </div>
         </div>
       </div>
