8d7408c2ea2a9118411e1473ad5de8e378f73c9e
sainideepanshu1 <114898317+sainideepanshu1@users.noreply.github.com>
2024-02-28 15:52:09 +0530

    Update Customers.jsx

--- src/Components/Customers.jsx
+++ src/Components/Customers.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { useRef } from "react";
 import { FaAngleDown } from "react-icons/fa";
 import { MdOutlineCurrencyRupee } from "react-icons/md";
+
 import { IoChevronUp } from "react-icons/io5";
 import { LuLayout } from "react-icons/lu";
 import { IoFilterSharp } from "react-icons/io5";
@@ -17,24 +18,23 @@ import { FaAngleLeft } from "react-icons/fa6";
 import axios from "axios";
 function Customers() {
   const [toggle, setToggle] = useState(false);
-  const imexRef = useRef(null);
   const [filter, setFilter] = useState(false);
-
-  const [checkbox,setcheckbox]=useState(false);
+  const imexRef = useRef(null);
+  const [checkbox, setcheckbox] = useState(false);
 
   const [customers, setCustomers] = useState([]);
 
   const fetchAllCustomers = async () => {
     try {
       const response = await axios.get(
-        'http://localhost:8000/api/customers/getCustomers'
+        "http://localhost:8000/api/customers/getCustomers"
       );
       const newCustomers = response.data.customers;
 
       setCustomers(newCustomers);
-      console.log(newCustomers);
+      console.log(newCustomers[3].addresses);
     } catch (error) {
-      console.error('Error fetching customers:', error);
+      console.error("Error fetching customers:", error);
     }
   };
 
@@ -42,15 +42,11 @@ function Customers() {
     fetchAllCustomers();
   }, []);
 
-
-
   // ---------click option--------
 
-
   function Option() {
-    setcheckbox(!checkbox)
+    setcheckbox(!checkbox);
   }
-
 
   const [sort, setSort] = useState(false);
   const sortRef = useRef(null);
@@ -58,11 +54,9 @@ function Customers() {
   // ----sort bar---
   const handleclick = (event) => {
     if (sortRef.current && !sortRef.current.contains(event.target)) {
-      setSort(false)
-
-
+      setSort(false);
     }
-  }
+  };
 
   useEffect(() => {
     document.addEventListener("click", handleclick, true);
@@ -71,7 +65,6 @@ function Customers() {
       document.removeEventListener("click", handleclick, true);
     };
   }, []);
-
 
   // ---------filter click-------
   function filterClick() {
@@ -141,7 +134,7 @@ function Customers() {
             </div>
             <button>
               <Link
-                to="/customers/add-customers"
+                to="/AddCustomers"
                 className="hover:bg-[#303030] bg-[#000000] text-[#F9FFFF] rounded-lg px-3 py-2 text-[12px]"
               >
                 Add customer
@@ -171,8 +164,9 @@ function Customers() {
             <div>
               <div
                 onClick={filterClick}
-                className={`flex transition-400 px-[15px] ${filter ? "hidden" : "block"
-                  }`}
+                className={`flex transition-400 px-[15px] ${
+                  filter ? "hidden" : "block"
+                }`}
               >
                 <button className="flex w-[89px] text-heading items-center px-[5px] py-[4px] gap-1 border-[1px]  bg-[#E3E3E3] hover:bg-[rgb(206,204,204)] rounded-lg ">
                   <span className="text-[#080808]">Add Filter</span>
@@ -183,8 +177,9 @@ function Customers() {
               </div>
               <div>
                 <div
-                  className={` flex items-center gap-8 px-[10px] ${filter ? "block" : "hidden"
-                    }`}
+                  className={` flex items-center gap-8 px-[10px] ${
+                    filter ? "block" : "hidden"
+                  }`}
                 >
                   <div className="cursor-pointer">
                     <div className="group  relative">
@@ -255,7 +250,7 @@ function Customers() {
         </div>
         {/* ------------------customer detail----------- */}
 
-        <div className=" bg-white rounded-xl border-[1px] border-[#a09f9fdc] shadow-md my-[20px] mx-7   xm:mx-0 gap-0 justify-normal xm:rounded-md xm:border-[0px] " >
+        <div className=" bg-white rounded-xl border-[1px] border-[#a09f9fdc] shadow-md my-[10px] mx-7   xm:mx-0 gap-0 justify-normal xm:rounded-md xm:border-[0px] ">
           <div className=" flex justify-between p-[8px]  ">
             <div className="w-full justify-center flex items-center  py-1 px-[8px] rounded-lg bg-[#faf8f8] focus-within:outline focus-within:bg-[#f1f1f1c2]  ">
               <span className="text-[#616161]">
@@ -279,7 +274,7 @@ function Customers() {
                 <button
                   ref={sortRef}
                   onClick={() => {
-                    setSort(!sort)
+                    setSort(!sort);
                   }}
                   className=" relative border-[1px] rounded-md bg-white p-[4px]"
                 >
@@ -292,46 +287,54 @@ function Customers() {
                   <div className="text-[#1a1a1a] flex flex-col gap-1 absolute top-[264px] right-[38px] px-[10px] rounded-lg py-[8px] bg-white text-[14px] xm:right-[10px]">
                     <h1>Sort by</h1>
                     <div className="  flex gap-2 outline-[#000] ">
-
-                      <label className="cursor-pointer"> <input
-                        className="text-[#1a1a1a] "
-                        type="radio"
-                        name="radio"
-                      ></input>
-                        Last update </label>
+                      <label className="cursor-pointer">
+                        {" "}
+                        <input
+                          className="text-[#1a1a1a] "
+                          type="radio"
+                          name="radio"
+                        ></input>
+                        Last update{" "}
+                      </label>
                     </div>
                     <div className="flex cursor-pointer gap-2">
                       <label className="cursor-pointer">
                         <input type="radio" value="HTML" name="radio" />
-                        Amount spent </label>
+                        Amount spent{" "}
+                      </label>
                     </div>
                     <div className="flex gap-2">
                       <label className="cursor-pointer">
                         <input type="radio" name="radio" />
-                        Total orders </label>
-                    </div>
-
-                    <div className="flex gap-2">
-                      <label className="cursor-pointer">
-                        <input type="radio" name="radio" />
-                        Last order date</label>
+                        Total orders{" "}
+                      </label>
                     </div>
 
                     <div className="flex gap-2">
                       <label className="cursor-pointer">
                         <input type="radio" name="radio" />
-                        First order date </label>
+                        Last order date
+                      </label>
                     </div>
 
                     <div className="flex gap-2">
                       <label className="cursor-pointer">
                         <input type="radio" name="radio" />
-                        Date added as customer </label>
+                        First order date{" "}
+                      </label>
+                    </div>
+
+                    <div className="flex gap-2">
+                      <label className="cursor-pointer">
+                        <input type="radio" name="radio" />
+                        Date added as customer{" "}
+                      </label>
                     </div>
                     <div className="flex gap-2">
                       <label className="cursor-pointer">
                         <input type="radio" name="radio" />
-                        Last abandoned order date </label>
+                        Last abandoned order date{" "}
+                      </label>
                     </div>
 
                     <div className="">
@@ -385,38 +388,29 @@ function Customers() {
               <div className="onclick customer option flex gap-[30px] text-[13px] px-3 py-2 xm:gap-1 xm:text-[12px] xm:px-1">
                 <div className=" px-[5px] py-[4px]  bg-[#E3E3E3] hover:bg-[rgb(206,204,204)] rounded-lg cursor-pointer">
                   <button>Merge customers</button>
-
                 </div>
 
                 <div className=" px-[5px] py-[4px]  bg-[#E3E3E3] hover:bg-[rgb(206,204,204)] rounded-lg cursor-pointer">
                   <button>Add tags</button>
-
                 </div>
                 <div className=" px-[5px] py-[4px]  bg-[#E3E3E3] hover:bg-[rgb(206,204,204)] rounded-lg cursor-pointer">
                   <button>Remove tags</button>
-
                 </div>
 
                 <div className=" px-[5px] py-[4px]  bg-[#E3E3E3] hover:bg-[rgb(206,204,204)] rounded-lg cursor-pointer">
                   <button>Delete customers</button>
-
                 </div>
-
-
-
               </div>
             </div>
-
-
           </div>
 
-          <div>
+          <div className="h-[420px] overflow-y-auto sm:hidden">
             {customers.map((customer, index) => (
               <div key={index}>
-                <Link to={`/customers/customer-details/${customer._id}`}>
+                <Link to={`/CustomerDetails/${customer._id}`}>
                   <div
                     className="grid justify-between gap-4 border-t border-b bg-white text-[#303030] px-[10px] py-[8px] text-[13px] font-medium items-center hover:bg-[#f5f3f3] cursor-pointer sm:hidden"
-                    style={{ gridTemplateColumns: '0fr 3fr 3fr 4fr 2fr 2fr' }}
+                    style={{ gridTemplateColumns: "0fr 3fr 3fr 4fr 2fr 2fr" }}
                   >
                     <div className="">
                       <input
@@ -432,21 +426,26 @@ function Customers() {
 
                     <div>
                       <button className="border-[1px] px-[5px] text-[12.5px] bg-[#b4fed2] text-[#0c5132] rounded-lg">
-                     {customer.verified_email ? ' Subscribed' : ""}
-                          
-                        
+                        {customer.verified_email ? " Subscribed" : ""}
                       </button>
                     </div>
 
                     <div>
-                      <h3>                     
-                        {customer.addresses.city ? `${customer.addresses.city}`: `${customer.addresses.country}`}
-                       
+                      <h3>
+                        {`${
+                          customer?.default_address?.city
+                            ? `${customer.default_address.city}, ${
+                                customer.default_address.province || ""
+                              }, ${customer.default_address.country}`
+                            : customer?.default_address?.province
+                            ? `${customer.default_address.province}, ${customer.default_address.country}`
+                            : customer?.default_address?.country ?? "-"
+                        }`}
                       </h3>
                     </div>
 
                     <div>
-                      <h3>{customer.orders_count}</h3>
+                      <h3>{`${customer.orders_count} orders`}</h3>
                     </div>
 
                     <div className="flex items-center">
@@ -462,43 +461,58 @@ function Customers() {
               </div>
             ))}
           </div>
+          <div className="hidden sm:block">
+            {customers.map((customer, index) => (
+              <div
+                key={index}
+                className="border-t border-b bg-white text-[#303030] px-[10px] py-[8px] text-[13px] font-medium items-center"
+              >
+                <div className="text-black text-[14px] py-[1px]">
+                  <h3>{`${customer.first_name} ${customer.last_name}`}</h3>
+                </div>
 
+                <div className=" py-[1px]">
+                  <h3>{`${
+                    customer?.default_address?.city
+                      ? `${customer.default_address.city}, ${
+                          customer.default_address.province || ""
+                        }, ${customer.default_address.country}`
+                      : customer?.default_address?.province
+                      ? `${customer.default_address.province}, ${customer.default_address.country}`
+                      : customer?.default_address?.country ?? "-"
+                  }`}</h3>
+                </div>
 
+                <div className="flex gap-1">
+                  <div>
+                    <h3>{`${customer.orders_count} orders`}</h3>
+                  </div>
 
-          <div className=" hidden  border-t border-b bg-white text-[#303030] px-[10px] py-[8px] text-[13px] font-medium items-center    sm:block">
-            <div className="text-black text-[14px] py-[1px]">
-              <h3>bablu lal</h3>
-            </div>
-
-            <div className=" py-[1px]">
-              <h3>India</h3>
-            </div>
-
-            <div className="flex gap-1">
-              <div>
-                <h3>0 Orders</h3>
-              </div>
-
-              <div className="flex items-center">
-                <span>
-                  <MdOutlineCurrencyRupee />
-                </span>
-                <div className="flex items-center">
-                  <h3>500</h3>
-                  <h2>.00</h2>
+                  <div className="flex items-center">
+                    <span>
+                      <MdOutlineCurrencyRupee />
+                    </span>
+                    <div className="flex items-center">
+                      <h3>{customer.total_spent}</h3>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
 
         <div className=" p-3  bg-[#f5f3f3] rounded-bl-xl rounded-br-xl ">
           <div className="flex justify-end ">
             <div className=" flex  gap-1 ">
-              <span className=" cursor-pointer  rounded-xl  p-[5px] text-[13px] hover:bg-[rgb(206,204,204)] " ><FaAngleLeft /></span>
-              <span className=" cursor-pointer p-[5px] text-[13px]   rounded-xl hover:bg-[rgb(206,204,204)] "> <FaAngleRight /></span>
+              <span className=" cursor-pointer  rounded-xl  p-[5px] text-[13px] hover:bg-[rgb(206,204,204)] ">
+                <FaAngleLeft />
+              </span>
+              <span className=" cursor-pointer p-[5px] text-[13px]   rounded-xl hover:bg-[rgb(206,204,204)] ">
+                {" "}
+                <FaAngleRight />
+              </span>
             </div>
-
           </div>
         </div>
       </div>
