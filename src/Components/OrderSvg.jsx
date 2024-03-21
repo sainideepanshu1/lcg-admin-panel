ab46f28fe6960197a88383c4c5b267ae0ec911a1
sainideepanshu1 <deepanshusaini.saini5@gmail.com>
2024-03-21 11:35:33 +0530

    fix

---
+++ src/Components/OrderSvg.jsx
@@ -0,0 +1,111 @@
+export default function OrderSvg() {
+  return (
+    <svg
+      width="110"
+      height="110"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M109.667 60.612c0 7.407-1.627 14.427-4.554 20.727-7.82 16.9-24.926 28.607-44.78 28.607-19.846 0-36.953-11.707-44.78-28.607C12.627 75.04 11 68.02 11 60.612 11 33.36 33.08 11.28 60.334 11.28c27.26 0 49.333 22.08 49.333 49.333Z"
+        fill="#F0F1F2"
+      />
+      <g filter="url(#a)">
+        <path
+          d="M97.667 28.333v55.684l-24 12.316h-25.47l-25.863-12v-72c0-.736.597-1.333 1.333-1.333h56.667v16c0 .736.597 1.333 1.333 1.333h16Z"
+          fill="#fff"
+        />
+        <path
+          d="M97.667 28.333h-16A1.333 1.333 0 0 1 80.334 27V11l17.333 17.333Z"
+          fill="#D2D5D9"
+        />
+      </g>
+      <path
+        d="M33 19.666h14.667"
+        stroke="#336FB2"
+        strokeWidth="6"
+        strokeLinecap="round"
+      />
+      <path
+        d="M55 28.333H32.334V51h22.667V28.333ZM55 58.333H32.334V81h22.667V58.333Z"
+        fill="#E8E9EB"
+      />
+      <path
+        d="M65 36.333h20.667"
+        stroke="#D2D5D9"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+      <path
+        d="M65 42.333h10.667"
+        stroke="#E8E9EB"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+      <path
+        d="M64.334 65.666h16"
+        stroke="#D2D5D9"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+      <path
+        d="M64.334 71.666h21.333"
+        stroke="#E8E9EB"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+      <path
+        d="m47.075 61 1.926 10.666H38.334L40.369 61h6.706Z"
+        fill="#EC856F"
+      />
+      <path d="m49 71.666-2 6.667h-6.666l-2-6.666h10.667Z" fill="#DE6A5A" />
+      <path
+        d="M51.667 36.333c0 1.054-.27 1.744-.641 2.667H35.64c-.37-.923-.641-1.613-.641-2.667h16.667Z"
+        fill="#399C97"
+      />
+      <path
+        d="M51.001 39c-1.202 3.514-4.18 6-7.666 6-3.486 0-6.465-2.487-7.667-6h15.333Z"
+        fill="#7FBEC6"
+      />
+      <path
+        d="M35.28 101.281h25c.733 0 1.333-.6 1.333-1.333v-.667h-4.626a1.34 1.34 0 0 1-1.267-.913l-4.727-14.18a1.333 1.333 0 0 0-1.266-.914H29.613l4.334 16.667c0 .74.6 1.34 1.333 1.34Z"
+        fill="#B6BABF"
+      />
+      <path d="M84.279 93.948H38.945v8.666H84.28v-8.666Z" fill="#5AB2AC" />
+      <path
+        d="M104.105 83.28a49.516 49.516 0 0 1-6.494 9.586l-.96 1.08c-9.02 9.833-21.98 16-36.373 16-14.393 0-27.353-6.167-36.373-16l-.96-1.08a49.479 49.479 0 0 1-6.494-9.587h28.2c.573 0 1.08.367 1.267.913l4.727 14.18c.18.547.693.914 1.266.914h19.414a1.34 1.34 0 0 0 1.266-.914l4.727-14.18c.18-.546.693-.913 1.267-.913h25.52Z"
+        fill="#399C97"
+      />
+      <defs>
+        <filter
+          id="a"
+          x="12.334"
+          y="3"
+          width="95.334"
+          height="105.333"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2" />
+          <feGaussianBlur stdDeviation="5" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.3 0" />
+          <feBlend
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_255_19371"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_255_19371"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
